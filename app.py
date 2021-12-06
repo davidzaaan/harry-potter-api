77d496cebf6e89ac9547fd464361fba2abe374df
david <odmh2711@gmail.com>
2021-12-06 17:15:27 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from hogwarts import characters
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    message = {'title': 'Hello world'}
+    return render_template('index.html')
+
+@app.route("/characters/")
+def chars():
+    hogwarts = characters()
+    return render_template('characters.html', chars=hogwarts)
+
+@app.route("/students/")
+def students():
+    return render_template('students.html', title="Students from...")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ hogwarts.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def characters():
+    response = requests.get("http://hp-api.herokuapp.com/api/characters")
+    if response.status_code != 200:
+        return False
+    characters = response.json()
+    return characters
+
+def students():
+    response = requests.get("http://hp-api.herokuapp.com/api/characters/students")
+    if response.status_code != 200:
+        return False
+    h_students = response.json()
+    return h_students
+
+def staff():
+    response = requests.get("http://hp-api.herokuapp.com/api/characters/staff")
+    if response.status_code != 200:
+        return False
+    h_staff = response.json()
+    return h_staff
